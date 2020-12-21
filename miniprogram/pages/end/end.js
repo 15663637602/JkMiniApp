c1b2ebd56efa70595d81b6d35bb43205ce81408b
15663637602 <243583762@qq.com>
2020-12-21 23:41:35 +0800

    样式微调

--- miniprogram/pages/end/end.js
+++ miniprogram/pages/end/end.js
@@ -14,14 +14,14 @@ Page({
     var animation = wx.createAnimation({
       duration: 15000,
       timingFunction: "ease",
-      delay: 6500,
+      delay: 6000,
       transformOrigin: '50% 50% 0'
     })
     animation.opacity(1).step();
     var animation2 = wx.createAnimation({
       duration: 15000,
       timingFunction: "ease",
-      delay: 6500,
+      delay: 6000,
       transformOrigin: '50% 50% 0'
     })
     animation2.opacity(0.8).step();
